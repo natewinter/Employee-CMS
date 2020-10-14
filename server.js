89812b112551a611157a5dbef24a7cfc70287933
Josh Brown <joshbrown2794@gmail.com>
2020-10-13 23:04:39 -0700

    fixed add employee functionality

--- server.js
+++ server.js
@@ -15,84 +15,93 @@ connection.connect(function (err) {
     start()
 })
 
-function start(){
+function start() {
     inquirer.prompt({
-        name:"options",
-        type:"list",
-        message:"please chose an option?",
-        choices:[ "add a department","add a role","add an employee","view departments","view roles","view employees","quit"]
-    }).then(function({options}){
-        if(options === "add a department"){
+        name: "options",
+        type: "list",
+        message: "please chose an option?",
+        choices: ["add a department", "add a role", "add an employee", "view departments", "view roles", "view employees", "quit"]
+    }).then(function ({
+        options
+    }) {
+        if (options === "add a department") {
             addDepartment()
-        }
-        else if(options === "add an employee") {
+        } else if (options === "add an employee") {
             addEmployee()
-        }
-        else if(options === "add a role"){
+        } else if (options === "add a role") {
             addRole()
-        }
-        else if(options === "view departments"){
+        } else if (options === "view departments") {
             allDepartments()
-        }else if(options === "view roles"){
+        } else if (options === "view roles") {
             allRoles()
-        }else if(options === "view employees"){
+        } else if (options === "view employees") {
             allEmployees()
-        }
-        
-        else{
+        } else {
             connection.end()
         }
     })
 }
+
 function allDepartments() {
     connection.query("SELECT * FROM departmentTb", function (err, data) {
         if (err) throw err
-           console.table(data) }
-)}
+        console.table(data)
+    })
+}
+
 function allRoles() {
     connection.query("SELECT * FROM roleTb", function (err, data) {
         if (err) throw err
-           console.table(data) }
-)}
+        console.table(data)
+    })
+}
+
 function allEmployees() {
     connection.query("SELECT * FROM employeeTb", function (err, data) {
         if (err) throw err
-           console.table(data) }
-)}
+        console.table(data)
+    })
+}
 
-function addDepartment(){
+function addDepartment() {
     inquirer.prompt({
-        name:"name",
-        type:"input",
-        message:"what is the name of the department you would like to add?"
-        }).then(function(answers){
-            connection.query("INSERT INTO departmentTb SET ?",{name:answers.name},function(err){
-                if (err) throw err
-                start()
-            })
+        name: "name",
+        type: "input",
+        message: "what is the name of the department you would like to add?"
+    }).then(function (answers) {
+        connection.query("INSERT INTO departmentTb SET ?", {
+            name: answers.name
+        }, function (err) {
+            if (err) throw err
+            start()
         })
+    })
 
 }
-function addRole(){
+
+function addRole() {
     inquirer.prompt({
-        name:"role",
-        type:"input",
-        message:"what is the role you would like to add?"
-        },{
-            name:"salary",
-            type:"input",
-            message:"what is the salary for this role?"
+            name: "role",
+            type: "input",
+            message: "what is the role you would like to add?"
+        }, {
+            name: "salary",
+            type: "input",
+            message: "what is the salary for this role?"
         },
 
 
-        ).then(function(answers){
-            connection.query("INSERT INTO roleTb SET ?",{title:answers.role},function(err){
-                if (err) throw err
-                start()
-            })
+    ).then(function (answers) {
+        connection.query("INSERT INTO roleTb SET ?", {
+            title: answers.role
+        }, function (err) {
+            if (err) throw err
+            start()
         })
+    })
 
 }
+
 function addEmployee() {
     connection.query("SELECT * FROM roleTb", function (err, data) {
         if (err) throw err
@@ -106,27 +115,25 @@ function addEmployee() {
 
 
 
-        inquirer.prompt([
-            {
-                name: "firstName",
-                type: "input",
-                message: "What is the first name of the employee you like to add??"
-            }, {
-                name: "lastName",
-                type: "input",
-                message: "what is the last name of the employee you'd like to add?"
-            },{
-                name:"managerid",
-                type: "confirm",
-                message:"is this employee a manager?"
+        inquirer.prompt([{
+            name: "firstName",
+            type: "input",
+            message: "What is the first name of the employee you like to add??"
+        }, {
+            name: "lastName",
+            type: "input",
+            message: "what is the last name of the employee you'd like to add?"
+        }, {
+            name: "managerid",
+            type: "confirm",
+            message: "is this employee a manager?"
 
-            },{
-                name: "roleId",
-                type: "list",
-                message: "What is the roll of this employee?",
-                choices: roleArr
-            }
-        ]).then(function (answers) {
+        }, {
+            name: "roleId",
+            type: "list",
+            message: "What is the roll of this employee?",
+            choices: roleArr
+        }]).then(function (answers) {
             connection.query("INSERT INTO employeeTb SET ?", {
                 first_name: answers.firstName,
                 last_name: answers.lastName,
